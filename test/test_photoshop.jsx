b22c02548731831decd1ff78f0b84352074f1623
Thomas Aylott <oblivious@subtlegradient.com>
2013-03-29 12:58:09 -0700

    Test runner for the Adobe ExtendScript engine

---
+++ test/test_photoshop.jsx
@@ -0,0 +1,36 @@
+﻿function main(){
+  
+  var exports = this.Spec = {}
+  #include "../vendor/spec/lib/spec.js"
+  // $.write("Spec: ")
+  // $.writeln(this.Spec)
+  
+  exports = this.Newton = {}
+  #include "../vendor/spec/lib/newton.js"
+  // $.write("Newton: ")
+  // $.writeln(this.Newton)
+  
+  exports = this.JSON = {}
+  #include "../lib/json3.js"
+  // $.write("JSON: ")
+  // $.writeln(this.JSON)
+  
+  try {
+    #include "./test_json3.js"
+  }
+  catch(e) {
+    $.bp()
+    print(e)
+    print(e.source.split('\n')[e.line-1])
+    // for (var property in e) {
+    //   print(property, ': ', e[property])
+    // }
+  }
+  
+}
+
+main.call({
+  load: function(identifier, path){
+    return this[identifier]
+  }
+});
